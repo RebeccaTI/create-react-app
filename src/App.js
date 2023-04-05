e8b970c45cbaae50deb928b0dc3414e3acf639ed
Rebecca de Moura Coelho Rangel <47149015+RebeccaTI@users.noreply.github.com>
2023-04-05 14:00:55 -0300

    Interpolação de variáveis e valores atribuidos de tags - estudo em react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Rebecca'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+ //alteração de produto jsx
+ const url ="https://via.placeholder.com/150"
+
+
   return (
     <div className="App">
      <h1>Olá React</h1>
-     <p>estou criando um projeto de react</p>
+     <p>Olá, meu nome é {newName}</p>
+     <p>Soma: {sum(1,2)}</p>
+     <img src={url} alt="Minha imagem" />
     </div>
   );
 }
